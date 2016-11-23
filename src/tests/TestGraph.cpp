f8b9acb1eb980db04b7e1eb20758cfb2ebac0372
Michael Song <songhaozhou@gmail.com>
2016-11-23 16:28:51 -0500

    Turned off testing, application starts on teach tab

--- src/tests/TestGraph.cpp
+++ src/tests/TestGraph.cpp
@@ -14,6 +14,7 @@ void TestGraph::testGraphLegacy()
     (*c).loadCSV("../Project Information/Sample Data/Teaching_sample.csv");
     QVERIFY((*c).getData() != empty);
 }
+
 void TestGraph::testGraphNew()
 {
     CSVReader *c = new CSVReader();
